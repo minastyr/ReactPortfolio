2cb909c711c7012c97112f5bcdabc52e2a9f171b
AnnuArchon <minastyr@gmail.com>
2025-03-26 19:09:52 -0400

    netlify continued work

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-/*   build: {
+  build: {
     outDir: 'dist', // Ensure this matches your desired output folder
-  }, */
+  },
   server: {
     port: 3000,
     open: true
